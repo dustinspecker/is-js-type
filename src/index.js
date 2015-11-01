8ed538dcbd5ffa99a58b641f7ced0b721f158c36
Dustin Specker <dustinspecker@dustinspecker.com>
2015-10-31 20:10:12 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+import jsTypes from 'js-types';
+
+const lowerCaseTypes = jsTypes.map(type => type.toLowerCase());
+
+/**
+ * Determine if a type is JS type
+ * @param {Stirng} type - type to verify
+ * @return {Boolean} - type is a JS type
+ */
+export default function isJsType(type) {
+  if (typeof type !== 'string') {
+    throw new TypeError('Expected type to be a string');
+  }
+
+  return lowerCaseTypes.indexOf(type.toLowerCase()) > -1;
+}
---
+++ test/test.js
@@ -0,0 +1,29 @@
+/* global describe, it */
+'use strict';
+import {expect} from 'chai';
+import isJsType from '../lib/';
+
+describe('is-js-type', () => {
+  it('should throw TypeError if type is not a string', () => {
+    function test() {
+      isJsType();
+    }
+
+    expect(test).to.throw(TypeError, /Expected type to be a string/);
+  });
+
+  it('should return false for non js types', () => {
+    expect(isJsType('gulp')).to.eql(false);
+    expect(isJsType('cat')).to.eql(false);
+  });
+
+  it('should return true for js types', () => {
+    expect(isJsType('Array')).to.eql(true);
+    expect(isJsType('Error')).to.eql(true);
+  });
+
+  it('should be case insensitive', () => {
+    expect(isJsType('array')).to.eql(true);
+    expect(isJsType('ARRAY')).to.eql(true);
+  });
+});
